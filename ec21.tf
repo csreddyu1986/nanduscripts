c9bce68404f800d57abf414372b6c5a9697f21a4
nandu-cloud <nandusn15@gmail.com>
2022-09-21 15:58:23 +0530

    my first commit

--- ec21.tf
+++ ec21.tf
@@ -2,12 +2,12 @@ provider "aws" {
   # I am using Virginia region
   region = "us-east-1"
 }
-resource "aws_instance" "testinstance" {
+resource "aws_instance" "testing" {
   ami           = "ami-05fa00d4c63e32376"
   instance_type = "t2.micro"
   key_name      = "terraformkey"
   tags = {
-    Name = "testinstance"
+    Name = "testing"
   }
 
 }
